af8c62c86cd894ec8a8723134b8a844a98b81e37
Maddy <maddy@savaslabs.com>
2021-10-25 12:59:43 -0400

    Accept pinata keys in body

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,13 @@ import pinataSDK from "@pinata/sdk";
 import fs from "fs";
 const cors = require("cors");
 const multer = require("multer");
-
 const app = express();
 const upload = multer({ dest: "uploads/" });
 const port = process.env.NODE_ENV === "production" ? process.env.PORT : 8080; // default port to listen
 let pinata: any;
-if (process.env.NODE_ENV === "production") {
-  pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_KEY);
-} else {
-  const PinataKeys = require("./PinataKeys");
-  pinata = pinataSDK(PinataKeys.apiKey, PinataKeys.apiSecret);
-}
 
 const corsOptions = {
-  origin: ["http://localhost:8082", "https://my-cool-nft-app.com"],
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
@@ -33,12 +25,13 @@ app.get("/", (req, res) => {
 // handles minting
 app.post("/mint", upload.single("image"), async (req, res) => {
   const multerReq = req as any;
-  //console.log(multerReq.file, req.body);
   if (!multerReq.file) {
     res.status(500).json({ status: false, msg: "no file provided" });
   } else {
     const fileName = multerReq.file.filename;
+
     // tests Pinata authentication
+    pinata = pinataSDK(req.body.apiKey, req.body.apiSecret);
     await pinata
       .testAuthentication()
       .catch((err: any) => res.status(500).json(JSON.stringify(err)));
@@ -48,9 +41,9 @@ app.post("/mint", upload.single("image"), async (req, res) => {
       pinataMetadata: {
         name: req.body.title.replace(/\s/g, "-"),
         keyvalues: {
-          description: req.body.description
-        }
-      }
+          description: req.body.description,
+        },
+      },
     };
     const pinnedFile = await pinata.pinFileToIPFS(
       readableStreamForFile,
@@ -70,13 +63,13 @@ app.post("/mint", upload.single("image"), async (req, res) => {
         decimals: 0,
         thumbnailUri: "https://tezostaquito.io/img/favicon.png",
         is_transferable: true,
-        shouldPreferSymbol: false
+        shouldPreferSymbol: false,
       };
 
       const pinnedMetadata = await pinata.pinJSONToIPFS(metadata, {
         pinataMetadata: {
-          name: "TUT-metadata"
-        }
+          name: "TUT-metadata",
+        },
       });
 
       if (pinnedMetadata.IpfsHash && pinnedMetadata.PinSize > 0) {
@@ -84,8 +77,8 @@ app.post("/mint", upload.single("image"), async (req, res) => {
           status: true,
           msg: {
             imageHash: pinnedFile.IpfsHash,
-            metadataHash: pinnedMetadata.IpfsHash
-          }
+            metadataHash: pinnedMetadata.IpfsHash,
+          },
         });
       } else {
         res
